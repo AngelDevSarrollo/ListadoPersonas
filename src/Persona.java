68363ef9c3f6227be786e3b817cca5864338efce
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-05-25 12:12:07 +0200

    encapsulamiento y sobreescritura en la clase persona

--- src/Persona.java
+++ src/Persona.java
@@ -16,4 +16,47 @@ public class Persona {
         this.tel = tel;
         this.email = email;
     }
+    //El metodo get nos permite obtener el valor de un atributo
+    public int getId(){
+        return id;
+    }
+    //El metodo set nos permite modificar el valor de un atributo
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getTel() {
+        return tel;
+    }
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                '}'+ super.toString();
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Pedro Sanchez", "435 685 865", "pedro_sepulturero@gmail.com");
+        System.out.println(persona1.toString());
+    }
 }
